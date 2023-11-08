8b8cf183eb99bba3760da156b4640a03b7def518
RyoTanifuji <tany_ryo@icloud.com>
2023-11-08 18:17:43 +0900

    Add: APIからユーザー登録できるように追加

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,11 @@
+class Api::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user
+    else
+      render json: user.errors, status: :bad_request
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'top#index'
+  namespace :api do
+    resources :users
+  end
 
   get '*path', to: 'top#index'
 end
