e1f72124faf9e892a09a1d041ed90ccfe4d1d702
RyoTanifuji <tany_ryo@icloud.com>
2023-11-10 22:39:01 +0900

    Add: rails-i18nでのモデル関連の日本語化

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,7 +5,7 @@ class Api::UsersController < ApplicationController
     if user.save
       render json: user
     else
-      render json: user.errors, status: :bad_request
+      render json: user.errors.full_messages, status: :bad_request
     end
   end
 
