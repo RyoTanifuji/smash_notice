c64202f7dce0e1223ab93186af311d01badb11af
RyoTanifuji <tany_ryo@icloud.com>
2023-11-16 19:18:10 +0900

    Remove: FightersテーブルからTimestampsに関するカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_16_044819) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_16_100340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "fighters", force: :cascade do |t|
     t.string "number"
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "folders", force: :cascade do |t|
