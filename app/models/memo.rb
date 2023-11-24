86aa92ece929c20198c93bba65f82a45d7d131f1
RyoTanifuji <tany_ryo@icloud.com>
2023-11-24 18:52:51 +0900

    Add: MatchupとStrategyのメモモデルの追加

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,7 +1,6 @@
 class Memo < ApplicationRecord
   belongs_to :user
   belongs_to :folder
-  belongs_to :fighter, optional: true
 
   validates :title, presence: true, length: { maximum: 20 }
   enum status: { private: 1, public: 2 }
